a8e7bc2d3d217bb1a941a2e5da8065ed838458da
Melissa Teh <melissathw97@gmail.com>
2025-07-13 15:43:00 +0800

    Fixed navbar button styling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "bg-lime-700/20 text-lime-900 shadow-xs hover:bg-lime-700/30 border",
+          "bg-lime-700/20 text-lime-900 shadow-xs hover:bg-lime-700/30",
         ghost:
           "hover:bg-accent text-black hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
