d019bc715d329a5dc0bd827cadab92de287bc699
Melissa Teh <melissathw97@gmail.com>
2025-07-15 21:03:51 +0800

    Auto redirect base path to dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
