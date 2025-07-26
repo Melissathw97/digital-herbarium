f7d1d9adef9f5318501d5a13717227793c269182
Melissa Teh <melissathw97@gmail.com>
2025-07-26 22:46:27 +0800

    Fixed Authentication layout

--- src/app/(auth)/users/layout.tsx
+++ src/app/(auth)/users/layout.tsx
@@ -6,20 +6,24 @@ export default function UsersLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen relative py-20 px-8 flex justify-center items-center">
-      <Image
-        alt="herbarium"
-        src="/herbarium-background.png"
-        quality={100}
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: "cover",
-          zIndex: "-10",
-        }}
-      />
-      <div className="bg-white m-auto p-10 w-full min-w-md max-w-lg rounded-md">
-        {children}
+    <div>
+      <div className="fixed w-screen h-screen">
+        <Image
+          alt="herbarium"
+          src="/herbarium-background.png"
+          quality={100}
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+            zIndex: "-10",
+          }}
+        />
+      </div>
+      <div className="min-h-screen relative py-20 px-8 flex justify-center items-center">
+        <div className="bg-white m-auto p-10 w-full min-w-md max-w-lg rounded-md z-10">
+          {children}
+        </div>
       </div>
     </div>
   );
