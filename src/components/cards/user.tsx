791cc8e6d7251b061e73c3b4b73902e058adec16
Melissa Teh <melissathw97@gmail.com>
2025-07-28 22:15:29 +0800

    Added border for current user in members list

--- src/components/cards/user.tsx
+++ src/components/cards/user.tsx
@@ -23,7 +23,9 @@ export default function UserCard({
   fullDetails?: boolean;
 }) {
   return (
-    <div className="bg-white shadow-sm rounded-sm px-5 py-4 border flex flex-col gap-1 justify-center">
+    <div
+      className={`${currentUser && "border border-lime-700"} bg-white shadow-sm rounded-sm px-5 py-4 border flex flex-col gap-1 justify-center`}
+    >
       <div className="flex gap-2">
         <div className="bg-gray-200 h-10 w-10 rounded-full grid place-items-center font-semibold text-gray-500 uppercase shrink-0">
           {name.substring(0, 1)}
@@ -78,9 +80,9 @@ export default function UserCard({
 
       {fullDetails && (
         <>
-          <hr className="mt-4" />
+          <hr className="mt-2" />
 
-          <div className="flex flex-col gap-2 mt-4 mb-2 text-xs text-gray-600">
+          <div className="flex flex-col gap-2 mt-2 text-xs text-gray-500">
             <div className="flex gap-2 items-center">
               <Mail className="h-3.5 w-3.5" />
               <p className="font-medium">{email}</p>
