1549add33b3703ecbf0b06c36b25976daf68a422
Melissa Teh <melissathw97@gmail.com>
2025-09-29 22:41:28 +0800

    Audit logs description - added column sorting

--- src/components/auditDescription.tsx
+++ src/components/auditDescription.tsx
@@ -1,8 +1,33 @@
 import { Fragment } from "react";
 import Badge from "./badge";
 import { Status } from "@/types/plant";
+import formatDate from "@/utils/formatDate";
 import UserRoleBadge from "./users/userRoleBadge";
 import { Log, LogAction, UserRole } from "@/types/user";
+
+const sortedColumns = [
+  "status",
+  "remarks",
+  "confidenceLevel",
+  "family",
+  "species",
+  "species_id",
+  "vernacular",
+  "barcode",
+  "prefix",
+  "number",
+  "collector",
+  "collected_at",
+  "state",
+  "district",
+  "location",
+  "elevation",
+  "latitude",
+  "longitude",
+  "additional_notes",
+  "is_published",
+  "organization",
+];
 
 export default function AuditDescription({ log }: { log: Log }) {
   const getBadgeVariant = (text: Status) => {
@@ -34,48 +59,100 @@ export default function AuditDescription({ log }: { log: Log }) {
             <Fragment key={log.id}>
               <br />
               <div className="mt-2 flex flex-col gap-1 text-[10px]">
-                {log.changes?.map(({ field, from, to }) => {
-                  const fieldName = field.replace("_", " ");
+                {log.changes
+                  ?.sort((a, b) => {
+                    const aIndex = sortedColumns.indexOf(a.field);
+                    const bIndex = sortedColumns.indexOf(b.field);
 
-                  if (field === "role") {
-                    return (
-                      <div
-                        key={`${log.dataId}-${field}`}
-                        className="flex items-center gap-0.5"
-                      >
-                        <span className="capitalize font-semibold text-gray-500">
-                          {fieldName}:
-                        </span>
-                        <UserRoleBadge user={{ role: from as UserRole }} />{" "}
-                        &rarr; <UserRoleBadge user={{ role: to as UserRole }} />
-                        <br />
-                      </div>
-                    );
-                  }
+                    // Handle fields that might not be in the PLANT_KEYS array
+                    // By defaulting them to a high number (e.g., Infinity), they are pushed to the end.
+                    const orderA = aIndex !== undefined ? aIndex : Infinity;
+                    const orderB = bIndex !== undefined ? bIndex : Infinity;
 
-                  if (field === "status") {
-                    return (
-                      <div key={`${log.dataId}-${field}`}>
-                        <span className="capitalize font-semibold text-gray-500">
-                          {fieldName}
-                        </span>
-                        : &nbsp;
-                        <Badge
-                          variant={getBadgeVariant(from as Status)}
-                          bordered
+                    return orderA - orderB;
+                  })
+                  .map(({ field, from, to }) => {
+                    const fieldName = field.replace("_", " ");
+
+                    if (field === "role") {
+                      return (
+                        <div
+                          key={`${log.dataId}-${field}`}
+                          className="flex items-center gap-0.5"
                         >
-                          {from || "-"}
-                        </Badge>{" "}
-                        &rarr;{" "}
-                        <Badge variant={getBadgeVariant(to as Status)} bordered>
-                          {to || "-"}
-                        </Badge>
-                        <br />
-                      </div>
-                    );
-                  }
+                          <span className="capitalize font-semibold text-gray-500">
+                            {fieldName}:
+                          </span>
+                          <UserRoleBadge user={{ role: from as UserRole }} />{" "}
+                          &rarr;{" "}
+                          <UserRoleBadge user={{ role: to as UserRole }} />
+                          <br />
+                        </div>
+                      );
+                    }
 
-                  if (field === "image_path") {
+                    if (field === "status") {
+                      return (
+                        <div key={`${log.dataId}-${field}`}>
+                          <span className="capitalize font-semibold text-gray-500">
+                            {fieldName}
+                          </span>
+                          : &nbsp;
+                          <Badge
+                            variant={getBadgeVariant(from as Status)}
+                            bordered
+                          >
+                            {from || "-"}
+                          </Badge>{" "}
+                          &rarr;{" "}
+                          <Badge
+                            variant={getBadgeVariant(to as Status)}
+                            bordered
+                          >
+                            {to || "-"}
+                          </Badge>
+                          <br />
+                        </div>
+                      );
+                    }
+
+                    if (field === "image_path") {
+                      return (
+                        <p
+                          key={`${log.dataId}-${field}`}
+                          className="overflow-hidden overflow-ellipsis"
+                        >
+                          <span className="capitalize font-semibold text-gray-500">
+                            {fieldName}
+                          </span>
+                          : &nbsp;
+                          {from ? (
+                            <span>
+                              {from.match(/\/[^\s\/]+$/)?.[0]} &rarr;{" "}
+                            </span>
+                          ) : null}
+                          {to.match(/\/[^\s\/]+$/)?.[0]}
+                          <br />
+                        </p>
+                      );
+                    }
+
+                    if (field === "collected_at") {
+                      return (
+                        <p key={`${log.dataId}-${field}`}>
+                          <span className="capitalize font-semibold text-gray-500">
+                            Date
+                          </span>
+                          : &nbsp;
+                          {from ? (
+                            <span>{formatDate(from)} &rarr; </span>
+                          ) : null}
+                          {formatDate(to)}
+                          <br />
+                        </p>
+                      );
+                    }
+
                     return (
                       <p
                         key={`${log.dataId}-${field}`}
@@ -85,30 +162,14 @@ export default function AuditDescription({ log }: { log: Log }) {
                           {fieldName}
                         </span>
                         : &nbsp;
-                        {from ? (
-                          <span>{from.match(/\/[^\s\/]+$/)?.[0]} &rarr; </span>
-                        ) : null}
-                        {to.match(/\/[^\s\/]+$/)?.[0]}
+                        {from ? <span>{from} &rarr; </span> : null}
+                        {to || (
+                          <span className="text-gray-400 italic">null</span>
+                        )}
                         <br />
                       </p>
                     );
-                  }
-
-                  return (
-                    <p
-                      key={`${log.dataId}-${field}`}
-                      className="overflow-hidden overflow-ellipsis"
-                    >
-                      <span className="capitalize font-semibold text-gray-500">
-                        {fieldName}
-                      </span>
-                      : &nbsp;
-                      {from ? <span>{from} &rarr; </span> : null}
-                      {to || <span className="text-gray-400 italic">null</span>}
-                      <br />
-                    </p>
-                  );
-                })}
+                  })}
               </div>
             </Fragment>
           )}
